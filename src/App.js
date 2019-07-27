e3741d7f5d4a983a52c79559588393a3fef99a18
Donny <donnymaster4@gmail.com>
2019-07-27 17:00:59 +0300

    reset

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Counter from './components/actionProduct.js';
-
 
 
 export default function(props){
     return (
         <div>
-            <Counter min={1} max={8} value={3}/>
-            <Counter min={4} max={12}/>
+           
         </div>
     )
 }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,4 @@ import ReactDom from 'react-dom';
 import App from './App.js';
  
 
-ReactDom.render(
-    <App />
-    , document.querySelector('.app'));
+ReactDom.render(<App />, document.querySelector('.app'));
